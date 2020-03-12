703554fc74c3321aab959be10c3f45d5c0158f28
Tim Hütz <tim+github@janke.biz>
2020-03-12 14:51:56 +0100

    update the selectors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use select::document::Document;
 use select::predicate::Name;
 
-static EXAMPLE_PAGE: &str = "https://www.tagesschau.de/ausland/italien-corona-115.html";
+static EXAMPLE_PAGE: &str = "https://www.tagesschau.de/ausland/italien-kliniken-101.html";
 
 fn main() {
     let res = reqwest::blocking::get(EXAMPLE_PAGE)
@@ -12,7 +12,22 @@ fn main() {
     Document::from(res.as_str())
         .find(Name("span"))
         .filter(|n| n.attr("class").is_some())
-        .filter(|n| n.attr("class").unwrap() == "stand")
-        .map(|n| n.as_text().unwrap())
+        .filter(|n| n.attr("class").unwrap().contains("stand"))
+        .map(|n| n.inner_html())
+        .for_each(|x| println!("{}", x));
+
+    Document::from(res.as_str())
+        .find(Name("p"))
+        .filter(|n| n.attr("class").is_some())
+        .filter(|n| n.attr("class").unwrap().contains("autorenzeile"))
+        .map(|n| n.inner_html())
+        .for_each(|x| println!("{}", x));
+
+
+    Document::from(res.as_str())
+        .find(Name("p"))
+        .filter(|n| n.attr("class").is_some())
+        .filter(|n| n.attr("class").unwrap().contains("autorenzeile"))
+        .map(|n| n.inner_html())
         .for_each(|x| println!("{}", x));
 }
